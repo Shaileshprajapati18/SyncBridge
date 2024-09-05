aff10e2f0f805a879c9fd6a3cc5173e906b905ae
Shaileshprajapati18 <shaileshprajapati182005@gmail.com>
2024-09-05 08:05:40 +0530

    update

--- app/src/main/java/com/example/myapplication/open_screen.java
+++ app/src/main/java/com/example/myapplication/open_screen.java
@@ -31,16 +31,20 @@ public class open_screen extends AppCompatActivity {
 
                 if (item.getItemId()==R.id.home_icon){
                     getSupportFragmentManager().beginTransaction().replace(R.id.open_screen,home).commit();
+                    return true;
                 }
                 else if (item.getItemId()==R.id.my_device){
                     getSupportFragmentManager().beginTransaction().replace(R.id.open_screen,my_device).commit();
+                    return true;
                 }
 
                 else if (item.getItemId()==R.id.other_device){
                     getSupportFragmentManager().beginTransaction().replace(R.id.open_screen,other_device).commit();
+                    return true;
                 }
                 else if (item.getItemId()==R.id.settings){
                     getSupportFragmentManager().beginTransaction().replace(R.id.open_screen,settings).commit();
+                    return true;
                 }
 
                 return false;
